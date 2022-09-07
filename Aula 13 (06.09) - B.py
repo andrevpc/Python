401b2b5d595563ec4b5c399527be5f1d15b0cdfd
andrevpc <111889239+andrevpc@users.noreply.github.com>
2022-09-07 18:40:12 -0300

    Add files via upload

--- Aula 13 (06.09) - B.py
+++ Aula 13 (06.09) - B.py
@@ -44,18 +44,34 @@
 # # media - desvio padrao azul e traço-traço
 # import numpy as np
 # import matplotlib.pyplot as plt
-# 
-# numeros = np.random.rand(20) * 20
-# print(numeros)
-# 
-# plt.plot(numeros)
-# plt.xticks(rotation='45')
+# y = np.random.rand(100)
+# x= np.linspace(0,20,100)
+# plt.plot(x,y, color = 'k')
+# plt.plot([np.mean(y)]*21, color = 'b')
+# plt.plot([np.mean(y)+np.std(y)]*21, color = 'b', linestyle = '--')
+# plt.plot([np.mean(y)-np.std(y)]*21, color = 'b', linestyle = '--')
 # plt.grid(linestyle='dashed')
 # plt.show()
 # =============================================================================
 
 # =============================================================================
-# # Exercício 4 - 
+# # Exercício 4 - Usar linha de meses e temp media max e min para fazer um gráfico
+# # preenchido
+# import matplotlib.pyplot as plt
+# meses = ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez']
+# temp_max = [26.8, 26.8, 26, 24, 20.8, 20.1, 19.7, 21.5, 21.4, 23.1, 25, 26.2]
+# temp_min = [17.2, 17.4, 16.5, 14.6, 11.2, 9.7, 9, 9.6, 11.1, 13.2, 14.9, 16.2]
+# fig,ax1 = plt.subplots(figsize=(10,4))
+# ax1.plot(meses, temp_max, marker = "o", color = 'r', label = "Temperatura Máxima")
+# ax1.plot(meses, temp_min, marker = "o", color = 'b', label = "Temperatura mínima")
+# plt.fill_between(meses, temp_max, temp_min, color = "indianred")
+# plt.fill_between(meses, temp_min, color = "royalblue")
+# plt.yticks([0, 15, 30])
+# plt.axis([0,11,0,30])
+# plt.xlabel("Meses")
+# plt.ylabel("Temperatura")
+# plt.grid(True, linewidth=1, linestyle='-')
+# =============================================================================
 
 # =============================================================================
 
@@ -76,23 +92,33 @@
 # plt.bar(("Vivos", "Mortos"), (vivo,morto))
 # =============================================================================
 
-# Exercício 2 - Quantidades de pessoas por quantidade de parentes a bordo e
-# quantidade por idade, diferenciandopor sexo
+# =============================================================================
+# # Exercício 2 - Quantidades de pessoas por quantidade de parentes a bordo
+# import pandas as pd
+# import matplotlib.pyplot as plt
+# titanic = pd.read_csv('titanic.csv')
+# 
+# titanic["Relatives"] = titanic["SibSp"] + titanic["Parch"]
+# passageiros_parente = titanic.groupby("Relatives")["PassengerId"].count()
+# passageiros_irmao = titanic.groupby("SibSp")["PassengerId"].count()
+# passageiros_filho = titanic.groupby("Parch")["PassengerId"].count()
+# print(passageiros_parente)
+# 
+# plt.plot(passageiros_parente,marker='o', label = 'Parentes')
+# plt.plot(passageiros_irmao,marker='o', label = 'Irmão')
+# plt.plot(passageiros_filho, marker='o', label = 'Filhos')
+# plt.legend()
+# plt.xlabel("Quantidade de parentes")
+# plt.ylabel("Quantidade de pessoas")
+# plt.grid(linestyle='-')
+# plt.show()
+# =============================================================================
+
+# Exercício 3 - Quantidade de pessoas por idade, diferenciando por sexo
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
 titanic = pd.read_csv('titanic.csv')
-
-titanic["Relatives"] = titanic["SibSp"] + titanic["Parch"]
-passageiros_parente = titanic.groupby("Relatives")["PassengerId"].count()
-passageiros_filho = titanic.groupby("SibSp")["PassengerId"].count()
-passageiros_irmao = titanic.groupby("Parch")["PassengerId"].count()
-print(passageiros_parente)
-
-plt.plot(titanic["Relatives"].count(),marker='o', label = 'Parentes')
-plt.plot(titanic["SibSp"].count(),marker='o', label = 'Irmão')
-plt.plot(titanic["Parch"].count(),marker='o', label = 'Pai')
-plt.legend()
-plt.xticks(rotation='45')
-plt.grid(linestyle='dashed')
-plt.show()
+x = titanic.groupby("Age")["PassengerId"].count()
+y = titanic.Age.count
+print(y)
+plt.plot(x)
