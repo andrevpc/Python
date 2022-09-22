ac4cb9a3bb0b369cfea90b97fec10680edf178e5
andrevpc <111889239+andrevpc@users.noreply.github.com>
2022-09-22 09:29:03 -0300

    Add files via upload

--- Aula 5 (25.08) - B.py
+++ Aula 5 (25.08) - B.py
@@ -101,43 +101,66 @@
 #                     break
 # =============================================================================
 
-# Desafio 2 - lig - 4 
-
-def tabuleiro(j):
-    print("          Jogador ", j)
-    print('  1  ','2  ','3  ','4  ','5  ','6  ','7')
-    for i in linhas:
-        for j in i:
-            print('| ',j,end='')
-        print("")
-
-def jogada(jogador):    
-    x = int(input("Escolha a coluna: "))
-    for i in range(5,-1,-1):                
-        if linhas[i][x-1] == ' ':
-            linhas[i][x-1] = jogador
-            break
-
+# =============================================================================
+# # Desafio 2 - lig - 4 
+# 
+# def tabuleiro(j):
+#     print("          Jogador ", j)
+#     print('  1  ','2  ','3  ','4  ','5  ','6  ','7')
+#     for i in linhas:
+#         for j in i:
+#             print('| ',j,end='')
+#         print("|")
+# 
+# def jogada(jogador):    
+#     x = int(input("Escolha a coluna: "))
+#     for i in range(5,-1,-1):                
+#         if linhas[i][x-1] == ' ':
+#             linhas[i][x-1] = jogador
+#             break
+# 
 # def vitoria():
-#     for i in limhas
-        
-lin1= [' ',' ',' ',' ',' ',' ',' ',' ']
-lin2= [' ',' ',' ',' ',' ',' ',' ',' ']
-lin3= [' ',' ',' ',' ',' ',' ',' ',' ']
-lin4= [' ',' ',' ',' ',' ',' ',' ',' ']
-lin5= [' ',' ',' ',' ',' ',' ',' ',' ']
-lin6= [' ',' ',' ',' ',' ',' ',' ',' ']
-
-linhas = [lin1,lin2,lin3,lin4,lin5,lin6]
-j = "x"
-
-tabuleiro(j)
-
-while 1:
-    if j == "o":
-        j="x"
-    else:
-        j="o"
-    
-    jogada(j)
-    tabuleiro(j)
+#     for i in linhas:
+#         for j in range(4):
+#             if i[j+0] == i[j+1] == i[j+2] == i[j+3] != " ":
+#                 return 1  
+# 
+#     for j in range(7):
+#         for i in range(3):
+#             if linhas[i][j] == linhas[i+1][j] == linhas[i+2][j] == linhas[i+3][j] != " ":
+#                 return 1
+# 
+#     for j in range(4):
+#         for i in range(3):
+#             if linhas[i][j] == linhas[i+1][j+1] == linhas[i+2][j+2] == linhas[i+3][j+3] != " ":
+#                 return 1
+#         
+#     for j in range(4):
+#         for i in range(3):
+#             if linhas[5-i][j] == linhas[4-i][j+1] == linhas[3-i][j+2] == linhas[2-i][j+3] != " ":
+#                 return 1
+#         
+# lin1= [' ',' ',' ',' ',' ',' ',' ']
+# lin2= [' ',' ',' ',' ',' ',' ',' ']
+# lin3= [' ',' ',' ',' ',' ',' ',' ']
+# lin4= [' ',' ',' ',' ',' ',' ',' ']
+# lin5= [' ',' ',' ',' ',' ',' ',' ']
+# lin6= [' ',' ',' ',' ',' ',' ',' ']
+# 
+# linhas = [lin1,lin2,lin3,lin4,lin5,lin6]
+# j = "x"
+# 
+# tabuleiro(j)
+# 
+# while 1:
+#     if j == "o":
+#         j="x"
+#     else:
+#         j="o"
+#     
+#     jogada(j)
+#     tabuleiro(j)
+#     if vitoria() == 1:
+#         print("Jogador x ganhou" if (j=="x") else "Jogador o ganhou")
+#         break
+# =============================================================================
