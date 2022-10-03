24ecfa1e36b18167d481e12b599618f8b7437749
andrevpc <111889239+andrevpc@users.noreply.github.com>
2022-10-03 11:57:34 -0300

    Add files via upload

---
+++ Aula 19 (03.10) - B - Recuperação.py
@@ -0,0 +1,128 @@
+# =============================================================================
+# # Exercício 1
+# def divisoes(lt,l1,l2,l3):
+#     for i in lt:
+#         separada = (list(str(i)))
+#         nomeseparado = separada[2:]
+#         if separada[0] == "1":
+#             l1.append("".join(nomeseparado))
+#         if separada[0] == "2":
+#             l2.append("".join(nomeseparado))
+#         if separada[0] == "3":
+#             l3.append("".join(nomeseparado))
+#     print('Primeira divisão: ', l1)
+#     print('Segunda divisão: ', l2)
+#     print('Terceira divisão: ', l3)
+#         
+# listatotal = ["1_palmeiras", "2_coritiba", "1_corinthians", "3_juventude", "2_fluminense", "3_bahia", "1_cuiaba", "2_cascavel", "3_ponte preta", "2_parana clube", "3_voltaredonda"]
+# lista1 = []
+# lista2 = []
+# lista3 = []
+# divisoes(listatotal, lista1, lista2, lista3)
+# =============================================================================
+
+# =============================================================================
+# # Exercício 2
+# import numpy as np
+# 
+# array = np.random.randint(0,10,25)
+# 
+# print('A array original:\n', array.reshape(5,5))
+# soma = 0
+# 
+# for i in range(len(array)):
+#     if array[i]%2 != 0:
+#         soma += 1
+#         
+# arquivo = open("arq.txt", "a")
+# arquivo.write(f"{soma}\n")  
+# arquivo.close()
+# 
+# array[array%2 != 0] = array[array%2 != 0]**2
+# 
+# arquivo = open("arq.txt", "r")
+# print("\nA array substituindo os números ímpares: \n", array.reshape(5,5))
+# print(f"Existem {len(arquivo.readlines())} registros.")
+# arquivo.close()
+# =============================================================================
+
+# =============================================================================
+# # Exercício 3
+# import numpy as np
+# 
+# def palindromo(frases):
+#     for i in range(frases):
+#         teste = (input(f"Insira a {i+1}° frase\n"))
+#         testemin = (list(teste.lower().replace(" ","")))
+#         if testemin == testemin[::-1]:
+#             frasespalin.append(teste)
+# 
+# frases = int(input("Quantas frases deseja verificar? "))
+# frasespalin = []
+# palindromo(frases)
+# print("\nSão palindromos:\n")
+# for i in frasespalin:
+#     print(i)
+# =============================================================================
+
+# =============================================================================
+# # Exercício 4
+# def notas(*lista,sit = False):
+#     total = len(lista)
+#     maior = max(lista)
+#     menor = min(lista)
+#     media = round((sum(lista)/total),2)
+#     if sit == True:
+#         return {"total": total, "maior": maior, "menor": menor, "média": media, "situacao": ("Boa" if media>7 else("Razoável" if 5<media<7 else "Ruim"))}
+# 
+#     return {"total": total, "maior": maior, "menor": menor, "média": media}
+# 
+# print(notas(5.5,2.5,1.5))
+# print(notas(5.5,2.5,9.9, sit=True))
+# =============================================================================
+
+# =============================================================================
+# # Exercício 5
+# # 1, 5, 10, 50 e 100
+# def caixa(v):
+#     if 10<v<600:
+#         n100 = v/100
+#         v = v - int(n100)*100
+#         n50 = v/50
+#         v = v - int(n50)*50
+#         n10 = v/10
+#         v = v - int(n10)*10
+#         n5 = v/5
+#         v = v - int(n5)*5
+#         n1 = v/1
+#         v = v - int(n1)
+#         print(n50)
+#         print("Notas para saque:")
+#         print(int(n100), " notas de R$100,00")
+#         print(int(n50), " notas de R$50,00")
+#         print(int(n10), " notas de R$10,00")
+#         print(int(n5), " notas de R$5,00")
+#         print(int(n1), " notas de R$1,00")
+#     else:
+#         print("Valor inválido")
+#         caixa(int(input("Novo valor: ")))
+# 
+# valor = int(input("Insira qual valor deseja sacar: (minímo R$10,00)\n"))
+# caixa(valor)
+# =============================================================================
+
+# =============================================================================
+# # Exercício 6
+# import pandas as pd
+# import matplotlib.pyplot as plt
+# 
+# sh = pd.read_csv("superheroes.csv", sep=',')
+# sh = sh.dropna()
+# forca = sh.groupby("creator")["strength_score"].mean()
+# forca = forca.reset_index()
+# 
+# plt.bar(forca["creator"], forca["strength_score"], color="k", zorder=2)
+# plt.xticks(rotation=45)
+# plt.ylabel("Strengh mean")
+# plt.grid(linestyle='-')
+# =============================================================================
